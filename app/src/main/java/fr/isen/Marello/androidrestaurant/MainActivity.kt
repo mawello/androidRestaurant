30d38c605425ea06cd2e7d91052538c835fb851a
mawello <64949958+mawello@users.noreply.github.com>
2021-01-25 17:20:09 +0100

    first

---
+++ app/src/main/java/fr/isen/Marello/androidrestaurant/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.Marello.androidrestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
